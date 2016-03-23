95a0652c02703760e5e7cccea72017c58b4a25ec
ssg <ssg@sourtimes.org>
2016-03-23 14:20:31 -0700

    fix NUnit test identification warnings

--- test/FnvTestVector.cs
+++ test/FnvTestVector.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) 2015, 2016 Sedat Kapanoglu
 // MIT License - see LICENSE file for details
 
+using System;
 namespace HashDepot.Test
 {
     public class FnvTestVector
@@ -8,5 +9,12 @@ namespace HashDepot.Test
         public byte[] Buffer { get; set; }
         public uint ExpectedResult32 { get; set; }
         public ulong ExpectedResult64 { get; set; }
+
+        public override string ToString()
+        {
+            return BitConverter.ToString(Buffer) 
+                + ExpectedResult32.ToString() 
+                + ExpectedResult64.ToString();
+        }
     }
 }
