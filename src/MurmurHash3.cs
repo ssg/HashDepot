416cef3934af35b5b1a34b5bccdf145c1e97adee
ssg <ssg@sourtimes.org>
2016-05-20 10:27:33 -0700

    start MurmurHash3 128-bit implementation

--- src/MurmurHash3.cs
+++ src/MurmurHash3.cs
@@ -60,5 +60,60 @@ namespace HashDepot
             hash ^= hash >> 16;
             return hash;
         }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static void murmur128Round(ref uint kn, ref uint hn, uint hm, uint cn, uint cm, uint n, int kr, int hr)
+        {
+            const uint m = 5;
+
+            kn *= cn;
+            kn = Bits.RotateLeft(kn, kr);
+            kn *= cm;
+            hn ^= kn;
+            hn = Bits.RotateLeft(hn, hr);
+            hn += hm;
+            hn = hn * m + n;
+        }
+
+        public static byte[] Hash128(byte[] buffer, uint seed)
+        {
+            const int outputLength = 16;
+            const int blockSize = 16;
+            const uint c1 = 0x239b961b;
+            const uint c2 = 0xab0e9789;
+            const uint c3 = 0x38b34ae5;
+            const uint c4 = 0xa1e38b93;
+
+            const uint n1 = 0x561ccd1b;
+            const uint n2 = 0x0bcaa747;
+            const uint n3 = 0x96cd1c35;
+            const uint n4 = 0x32ac3b17;
+
+            var result = new byte[outputLength];
+
+            uint h1 = seed;
+            uint h2 = seed;
+            uint h3 = seed;
+            uint h4 = seed;
+
+            int length = buffer.Length;
+            int end = length - length % blockSize;
+
+            for (int i = 0; i < end; i+= blockSize)
+            {
+                int offset = i * 4;
+                uint k1 = BitConverter.ToUInt32(buffer, offset);
+                uint k2 = BitConverter.ToUInt32(buffer, offset + 4);
+                uint k3 = BitConverter.ToUInt32(buffer, offset + 8);
+                uint k4 = BitConverter.ToUInt32(buffer, offset + 12);
+
+                murmur128Round(ref k1, ref h1, h2, c1, c2, n1, 15, 19);
+                murmur128Round(ref k2, ref h2, h3, c2, c3, n2, 16, 17);
+                murmur128Round(ref k3, ref h3, h4, c3, c4, n3, 17, 15);
+                murmur128Round(ref k4, ref h4, h1, c4, c1, n4, 18, 13);
+            }
+            /// not finished
+            return result;
+        }
     }
 }
