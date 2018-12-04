0e7bba8509e7cb3c357eca753ccb27774fcb98e3
Sedat Kapanoglu <ssg@sourtimes.org>
2018-12-04 15:43:22 -0800

    consolidate nuspec and AssemblyInfo into csproj

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -1,23 +1,3 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.CompilerServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("HashDepot")]
-[assembly: AssemblyDescription("HashDepot hash functions library")]
-[assembly: AssemblyProduct("HashDepot")]
-[assembly: AssemblyCopyright("Copyright © 2015-2016 Sedat Kapanoglu")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("020ceffe-d464-44b0-8764-eaf2cc80718d")]
-
-[assembly: AssemblyVersion("2.0.0")]
-[assembly: AssemblyFileVersion("2.0.0")]
 [assembly: InternalsVisibleTo("HashDepotTest")]
