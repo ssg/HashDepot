b197b321ae09667e41cb21c6ceb33f3a88380f8f
Sedat Kapanoglu <ssg@sourtimes.org>
2019-06-07 13:53:35 -0700

    fix xxHash bugs

--- src/XXHash.cs
+++ src/XXHash.cs
@@ -13,11 +13,17 @@ namespace HashDepot
     /// </summary>
     public static class XXHash
     {
-        private const ulong prime641 = 11400714785074694791ul;
-        private const ulong prime642 = 14029467366897019727ul;
-        private const ulong prime643 = 1609587929392839161ul;
-        private const ulong prime644 = 9650029242287828579ul;
-        private const ulong prime645 = 2870177450012600261ul;
+        private const ulong prime64v1 = 11400714785074694791ul;
+        private const ulong prime64v2 = 14029467366897019727ul;
+        private const ulong prime64v3 = 1609587929392839161ul;
+        private const ulong prime64v4 = 9650029242287828579ul;
+        private const ulong prime64v5 = 2870177450012600261ul;
+
+        private const uint prime32v1 = 2654435761u;
+        private const uint prime32v2 = 2246822519u;
+        private const uint prime32v3 = 3266489917u;
+        private const uint prime32v4 = 668265263u;
+        private const uint prime32v5 = 374761393u;
 
         /// <summary>
         /// Generate a 32-bit xxHash value.
@@ -27,14 +33,13 @@ namespace HashDepot
         /// <returns>32-bit hash value.</returns>
         public static unsafe uint Hash32(ReadOnlySpan<byte> buffer, uint seed = 0)
         {
-            const uint prime1 = 2654435761u;
-            const uint prime2 = 2246822519u;
-            const uint prime3 = 3266489917u;
-            const uint prime4 = 668265263u;
-            const uint prime5 = 374761393u;
-
             const int stripeLength = 16;
             const int laneLength = 4;
+
+            if (!BitConverter.IsLittleEndian)
+            {
+                throw new NotSupportedException("Big endian not supported yet");
+            }
 
             int len = buffer.Length;
             int remainingLen = len;
@@ -46,23 +51,23 @@ namespace HashDepot
 
                 if (len < stripeLength)
                 {
-                    acc = seed + prime5;
+                    acc = seed + prime32v5;
                     goto Skip;
                 }
 
-                uint acc1 = seed + prime1 + prime2;
-                uint acc2 = seed + prime2;
+                uint acc1 = seed + prime32v1 + prime32v2;
+                uint acc2 = seed + prime32v2;
                 uint acc3 = seed;
-                uint acc4 = seed - prime1;
+                uint acc4 = seed - prime32v1;
 
                 do
                 {
                     void processLane(ref uint accn)
                     {
                         uint lane = *(uint*)pInput;
-                        accn += lane * prime2;
+                        accn += lane * prime32v2;
                         accn = Bits.RotateLeft(accn, 13);
-                        accn *= prime1;
+                        accn *= prime32v1;
                         pInput += laneLength;
                     }
 
@@ -85,24 +90,18 @@ namespace HashDepot
                 for (uint lane; remainingLen >= laneLength; remainingLen -= laneLength, pInput += laneLength)
                 {
                     lane = *(uint*)pInput;
-                    acc += lane * prime3;
-                    acc = Bits.RotateLeft(acc, 11) * prime4;
+                    acc += lane * prime32v3;
+                    acc = Bits.RotateLeft(acc, 17) * prime32v4;
                 }
 
                 for (byte lane; remainingLen >= 1; remainingLen--, pInput++)
                 {
                     lane = *pInput;
-                    acc += lane * prime5;
-                    acc = Bits.RotateLeft(acc, 11) * prime1;
+                    acc += lane * prime32v5;
+                    acc = Bits.RotateLeft(acc, 11) * prime32v1;
                 }
 
-                acc ^= acc >> 15;
-                acc *= prime2;
-                acc ^= acc >> 13;
-                acc *= prime3;
-                acc ^= acc >> 16;
-
-                return acc;
+                return avalanche32(acc);
             }
         }
 
@@ -117,6 +116,11 @@ namespace HashDepot
             const int stripeLength = 32;
             const int laneLength = 8;
 
+            if (!BitConverter.IsLittleEndian)
+            {
+                throw new NotSupportedException("Big endian not supported yet");
+            }
+
             int len = buffer.Length;
             int remainingLen = len;
             ulong acc;
@@ -127,14 +131,14 @@ namespace HashDepot
 
                 if (len < stripeLength)
                 {
-                    acc = seed + prime645;
+                    acc = seed + prime64v5;
                     goto Skip;
                 }
 
-                ulong acc1 = seed + prime641 + prime642;
-                ulong acc2 = seed + prime642;
+                ulong acc1 = seed + prime64v1 + prime64v2;
+                ulong acc2 = seed + prime64v2;
                 ulong acc3 = seed;
-                ulong acc4 = seed - prime641;
+                ulong acc4 = seed - prime64v1;
 
                 do
                 {
@@ -171,48 +175,62 @@ namespace HashDepot
                 {
                     lane = *(ulong*)pInput;
                     acc ^= round64(0, lane);
-                    acc = Bits.RotateLeft(acc, 27) * prime641;
-                    acc += prime644;
+                    acc = Bits.RotateLeft(acc, 27) * prime64v1;
+                    acc += prime64v4;
                 }
 
                 for (uint lane; remainingLen >= 4; remainingLen -= 4, pInput += 4)
                 {
                     lane = *(uint*)pInput;
-                    acc ^= lane * prime641;
-                    acc = Bits.RotateLeft(acc, 23) * prime642;
-                    acc += prime643;
+                    acc ^= lane * prime64v1;
+                    acc = Bits.RotateLeft(acc, 23) * prime64v2;
+                    acc += prime64v3;
                 }
 
                 for (byte lane; remainingLen >= 1; remainingLen--, pInput++)
                 {
                     lane = *pInput;
-                    acc ^= lane * prime645;
-                    acc = Bits.RotateLeft(acc, 11) * prime641;
+                    acc ^= lane * prime64v5;
+                    acc = Bits.RotateLeft(acc, 11) * prime64v1;
                 }
 
-                acc ^= acc >> 33;
-                acc *= prime642;
-                acc ^= acc >> 29;
-                acc *= prime643;
-                acc ^= acc >> 32;
-
-                return acc;
+                return avalanche64(acc);
             }
+        }
+
+        private static ulong avalanche64(ulong acc)
+        {
+            acc ^= acc >> 33;
+            acc *= prime64v2;
+            acc ^= acc >> 29;
+            acc *= prime64v3;
+            acc ^= acc >> 32;
+            return acc;
+        }
+
+        private static uint avalanche32(uint acc)
+        {
+            acc ^= acc >> 15;
+            acc *= prime32v2;
+            acc ^= acc >> 13;
+            acc *= prime32v3;
+            acc ^= acc >> 16;
+            return acc;
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static ulong round64(ulong accn, ulong lane)
         {
-            accn += lane * prime642;
-            return Bits.RotateLeft(accn, 31);
+            accn += lane * prime64v2;
+            return Bits.RotateLeft(accn, 31) * prime64v1;
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static void mergeAccumulator64(ref ulong acc, ulong accn)
         {
             acc ^= round64(0, accn);
-            acc *= prime641;
-            acc += prime644;
+            acc *= prime64v1;
+            acc += prime64v4;
         }
     }
 }
---
+++ test/XXHashTest.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using NUnit.Framework;
+
+namespace HashDepot.Test
+{
+    [TestFixture]
+    public class XXHashTest
+    {
+        // test values are from https://asecuritysite.com/encryption/xxHash
+        private static readonly object[] testVectors = new[]
+        {
+            new object[] { "",                                              0U,     0x02cc5d05U, 0xef46db3751d8e999UL },
+            new object[] { "a",                                             0U,     0x550d7456U, 0xd24ec4f1a98c6e5bUL },
+            new object[] { "123",                                           0U,     0xb6855437U, 0x3c697d223fa7e885UL },
+            new object[] { "1234",                                          0U,     0x01543429U, 0xd8316e61d84f6ba4UL },
+            new object[] { "123456789012345",                               0U,     0xda7b17e8U, 0xc377d78ade001a3cUL },
+            new object[] { "Nobody inspects the spammish repetition",       0u,     0xe2293b2fU, 0xfbcea83c8a378bf1UL },
+            new object[] { "Nobody inspects the spammish repetition",       123u,   0xfa7f6052U, 0xa8ba45551f24b7aeUL },
+            new object[] { "The quick brown fox jumps over the lazy dog",   0u,     0xe85ea4deU, 0x0b242d361fda71bcUL },
+        };
+
+        [Test]
+        [TestCaseSource(nameof(testVectors))]
+        public void Hash32_BinaryTests(string text, uint seed, uint hash32, ulong _)
+        {
+            var buffer = Encoding.UTF8.GetBytes(text);
+            var result = XXHash.Hash32(buffer, seed);
+            Assert.AreEqual(hash32, result);
+        }
+
+        [Test]
+        [TestCaseSource(nameof(testVectors))]
+        public void Hash64_BinaryTests(string text, uint seed, uint _, ulong hash64)
+        {
+            var buffer = Encoding.UTF8.GetBytes(text);
+            var result = XXHash.Hash64(buffer, seed);
+            Assert.AreEqual(hash64, result);
+        }
+    }
+}
