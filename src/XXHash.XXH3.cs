933627c6b3bf1a86b02a536922f22e985a5a8cef
Sedat Kapanoglu <sedat@kapanoglu.com>
2025-09-15 19:05:50 -0700

    initial XXH3 work

---
+++ src/XXHash.XXH3.cs
@@ -0,0 +1,395 @@
+﻿// <copyright file="XXHash.XXH3.cs" company="Sedat Kapanoglu">
+// Copyright (c) 2015-2025 Sedat Kapanoglu
+// MIT License (see LICENSE file for details)
+// </copyright>
+
+using System;
+using System.Buffers.Binary;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+
+namespace HashDepot;
+
+/// <summary>
+/// XXH3 implementation.
+/// </summary>
+static partial class XXHash
+{
+    /// <summary>
+    /// 64-bit XXH3 implementation.
+    /// </summary>
+    public static class XXH3
+    {
+        internal const int MinSecretSize = 136;
+        internal const int SecretDefaultSize = 192;
+        internal const int MaxSecretSize = 9867;
+
+        const int stripeLength = 64;
+        const int secretConsumeRate = 8;
+        const int accNumBlocks = stripeLength / sizeof(ulong);
+
+        const ulong prime64mx1 = 0x165667919E3779F9UL;
+        const ulong prime64mx2 = 0x9FB21C651E98DF25UL;
+
+        const int midSizeMax = 240;
+
+        const int secretLastAccStart = 7;
+        const int secretMergeAccsStart = 11;
+
+        static readonly byte[] kSecret =
+        [
+            0xb8, 0xfe, 0x6c, 0x39, 0x23, 0xa4, 0x4b, 0xbe, 0x7c, 0x01, 0x81, 0x2c, 0xf7, 0x21, 0xad, 0x1c,
+            0xde, 0xd4, 0x6d, 0xe9, 0x83, 0x90, 0x97, 0xdb, 0x72, 0x40, 0xa4, 0xa4, 0xb7, 0xb3, 0x67, 0x1f,
+            0xcb, 0x79, 0xe6, 0x4e, 0xcc, 0xc0, 0xe5, 0x78, 0x82, 0x5a, 0xd0, 0x7d, 0xcc, 0xff, 0x72, 0x21,
+            0xb8, 0x08, 0x46, 0x74, 0xf7, 0x43, 0x24, 0x8e, 0xe0, 0x35, 0x90, 0xe6, 0x81, 0x3a, 0x26, 0x4c,
+            0x3c, 0x28, 0x52, 0xbb, 0x91, 0xc3, 0x00, 0xcb, 0x88, 0xd0, 0x65, 0x8b, 0x1b, 0x53, 0x2e, 0xa3,
+            0x71, 0x64, 0x48, 0x97, 0xa2, 0x0d, 0xf9, 0x4e, 0x38, 0x19, 0xef, 0x46, 0xa9, 0xde, 0xac, 0xd8,
+            0xa8, 0xfa, 0x76, 0x3f, 0xe3, 0x9c, 0x34, 0x3f, 0xf9, 0xdc, 0xbb, 0xc7, 0xc7, 0x0b, 0x4f, 0x1d,
+            0x8a, 0x51, 0xe0, 0x4b, 0xcd, 0xb4, 0x59, 0x31, 0xc8, 0x9f, 0x7e, 0xc9, 0xd9, 0x78, 0x73, 0x64,
+            0xea, 0xc5, 0xac, 0x83, 0x34, 0xd3, 0xeb, 0xc3, 0xc5, 0x81, 0xa0, 0xff, 0xfa, 0x13, 0x63, 0xeb,
+            0x17, 0x0d, 0xdd, 0x51, 0xb7, 0xf0, 0xda, 0x49, 0xd3, 0x16, 0x55, 0x26, 0x29, 0xd4, 0x68, 0x9e,
+            0x2b, 0x16, 0xbe, 0x58, 0x7d, 0x47, 0xa1, 0xfc, 0x8f, 0xf8, 0xb8, 0xd1, 0x7a, 0xd0, 0x31, 0xce,
+            0x45, 0xcb, 0x3a, 0x8f, 0x95, 0x16, 0x04, 0x28, 0xaf, 0xd7, 0xfb, 0xca, 0xbb, 0x4b, 0x40, 0x7e,
+        ];
+
+        static readonly ulong[] initAcc =
+        [
+            prime32v3, prime64v1, prime64v2, prime64v3,
+            prime64v4, prime32v2, prime64v5, prime32v1
+        ];
+
+        /// <summary>
+        /// Hash given bytes using XXH3 64-bit algorithm with the default secret.
+        /// </summary>
+        /// <param name="input">Input bytes.</param>
+        /// <param name="seed">Optional seed value.</param>
+        /// <returns>Hash result.</returns>
+        public static ulong Hash64(ReadOnlySpan<byte> input, ulong seed = 0)
+        {
+            return hash64Internal(input, seed, kSecret);
+        }
+
+        /// <summary>
+        /// Hash given bytes using XXH3 64-bit algorithm with given secret.
+        /// </summary>
+        /// <param name="input">Input bytes.</param>
+        /// <param name="secret">Secret.</param>
+        /// <param name="seed">Optional seed.</param>
+        /// <returns>Hash result.</returns>
+        public static ulong Hash64(ReadOnlySpan<byte> input, ReadOnlySpan<byte> secret, ulong seed = 0)
+        {
+            return hash64Internal(input, seed, secret);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static ulong mul128fold64(ulong a, ulong b)
+        {
+            UInt128 a128 = a;
+            UInt128 b128 = b;
+            UInt128 product = a128 * b128;
+            return (ulong)((product & 0xFFFFFFFF_FFFFFFFFUL) ^ (product >> 64));
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static ulong len1To3(ReadOnlySpan<byte> input, ReadOnlySpan<byte> secret, ulong seed)
+        {
+            byte c1 = input[0];
+            byte c2 = input[input.Length >> 1];
+            byte c3 = input[^1];
+            uint combined = ((uint)c1 << 16)
+                | ((uint)c2 << 24)
+                | ((uint)c3 << 0)
+                | ((uint)input.Length << 8);
+            ulong bitflip = (BitConverter.ToUInt32(secret) ^ BitConverter.ToUInt32(secret[4..])) + seed;
+            ulong keyed = combined ^ bitflip;
+            return avalanche(keyed);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static ulong len4To8(ReadOnlySpan<byte> input, ReadOnlySpan<byte> secret, ulong seed)
+        {
+            seed ^= BinaryPrimitives.ReverseEndianness((uint)seed) << 32;
+
+            uint input1 = BitConverter.ToUInt32(input);
+            uint input2 = BitConverter.ToUInt32(input[^4..]);
+            ulong bitflip = (BitConverter.ToUInt64(secret[8..]) ^ BitConverter.ToUInt64(secret[16..])) - seed;
+            ulong input64 = input2 + ((ulong)input1 << 32);
+            ulong keyed = input64 ^ bitflip;
+            return rrmxmx64(keyed, (ulong)input.Length);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static ulong len9To16(ReadOnlySpan<byte> input, ReadOnlySpan<byte> secret, ulong seed)
+        {
+            ulong bitflip1 = (BitConverter.ToUInt64(secret[24..]) ^ BitConverter.ToUInt64(secret[32..])) + seed;
+            ulong bitflip2 = (BitConverter.ToUInt64(secret[40..]) ^ BitConverter.ToUInt64(secret[48..])) - seed;
+            ulong inputLo = BitConverter.ToUInt64(input) ^ bitflip1;
+            ulong inputHi = BitConverter.ToUInt64(input[^8..]) ^ bitflip2;
+            ulong acc = (ulong)input.Length
+                + BinaryPrimitives.ReverseEndianness(inputLo)
+                + mul128fold64(inputLo, inputHi);
+            return avalanche(acc);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static ulong len0To16(ReadOnlySpan<byte> input, ReadOnlySpan<byte> secret, ulong seed)
+        {
+            if (input.Length > 8)
+            {
+                return len9To16(input, secret, seed);
+            }
+
+            if (input.Length >= 4)
+            {
+                return len4To8(input, secret, seed);
+            }
+
+            if (input.Length > 0)
+            {
+                return len1To3(input, secret, seed);
+            }
+
+            return avalanche(seed
+                ^ (BitConverter.ToUInt64(secret[56..])
+                ^ BitConverter.ToUInt64(secret[64..])));
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static ulong mix16b(ReadOnlySpan<byte> input, ReadOnlySpan<byte> secret, ulong seed)
+        {
+            ulong inputLo = BitConverter.ToUInt64(input);
+            ulong inputHi = BitConverter.ToUInt64(input[8..]);
+            return mul128fold64(
+                inputLo ^ (BitConverter.ToUInt64(secret) + seed),
+                inputHi ^ (BitConverter.ToUInt64(secret[8..]) - seed));
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static ulong rrmxmx64(ulong acc, ulong len)
+        {
+            acc ^= BitOperations.RotateLeft(acc, 49) ^ BitOperations.RotateLeft(acc, 24);
+            acc *= prime64mx2;
+            acc ^= (acc >> 35) + len;
+            acc *= prime64mx2;
+            return Bits.XxhXorShift64(acc, 28);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static ulong len17to128(ReadOnlySpan<byte> input, ReadOnlySpan<byte> secret, ulong seed)
+        {
+            ulong acc = (ulong)input.Length * prime64v1;
+            if (input.Length > 32)
+            {
+                if (input.Length > 64)
+                {
+                    if (input.Length > 96)
+                    {
+                        acc += mix16b(input[48..], secret[96..], seed);
+                        acc += mix16b(input[^64..], secret[112..], seed);
+                    }
+
+                    acc += mix16b(input[32..], secret[64..], seed);
+                    acc += mix16b(input[^48..], secret[80..], seed);
+                }
+
+                acc += mix16b(input[16..], secret[32..], seed);
+                acc += mix16b(input[^32..], secret[48..], seed);
+            }
+
+            acc += mix16b(input, secret, seed);
+            acc += mix16b(input[^16..], secret[16..], seed);
+
+            return avalanche(acc);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static ulong len129to240(ReadOnlySpan<byte> input, ReadOnlySpan<byte> secret, ulong seed)
+        {
+            const int midsizeStartOffset = 3;
+            const int midsizeLastOffset = 17;
+
+            ulong acc = (ulong)input.Length * prime64v1;
+
+            int nbBlocks = input.Length / 16;
+            for (int i = 0; i < 8; i++)
+            {
+                acc += mix16b(input[(i * 16)..], secret[(i * 16)..], seed);
+            }
+
+            ulong accEnd = mix16b(input[^16..], secret[(MinSecretSize - midsizeLastOffset)..], seed);
+
+            acc = avalanche(acc);
+
+            for (int i = 8; i < nbBlocks; i++)
+            {
+                accEnd += mix16b(input[(i * 16)..], secret[((i * 16) - 8 + midsizeStartOffset)..], seed);
+            }
+
+            return avalanche(acc + accEnd);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static ulong avalanche(ulong acc)
+        {
+            acc = Bits.XxhXorShift64(acc, 37);
+            acc *= prime64mx1;
+            acc = Bits.XxhXorShift64(acc, 32);
+            return acc;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static ulong mul32To64Add64(ulong a, ulong b, ulong acc)
+        {
+            uint a32 = (uint)a;
+            uint b32 = (uint)b;
+            return (a32 * (ulong)b32) + acc;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static void round(Span<ulong> acc, ReadOnlySpan<byte> input, ReadOnlySpan<byte> secret, int lane)
+        {
+            ulong dataVal = BitConverter.ToUInt64(input[(lane * 8)..]);
+            ulong dataKey = dataVal ^ BitConverter.ToUInt64(secret[(lane * 8)..]);
+            acc[lane ^ 1] = dataVal;
+            acc[lane] = mul32To64Add64(dataKey, dataKey >> 32, acc[lane]);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static void accumulate512(Span<ulong> acc, ReadOnlySpan<byte> input, ReadOnlySpan<byte> secret)
+        {
+            for (int i = 0; i < accNumBlocks; i++)
+            {
+                round(acc, input, secret, i);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static void scrambleRound(Span<ulong> acc, ReadOnlySpan<byte> secret, int lane)
+        {
+            ulong key64 = BitConverter.ToUInt64(secret[(lane * 8)..]);
+            ulong acc64 = acc[lane];
+            acc64 = Bits.XxhXorShift64(acc64, 47);
+            acc64 ^= key64;
+            acc64 *= prime32v1;
+            acc[lane] = acc64;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static void scrambleAcc(Span<ulong> acc, ReadOnlySpan<byte> secret)
+        {
+            for (int i = 0; i < accNumBlocks; i++)
+            {
+                scrambleRound(acc, secret, i);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static void initCustomSecret(Span<byte> customSecret, ulong seed64)
+        {
+            const int nbRounds = SecretDefaultSize / 16;
+            for (int i = 0; i < nbRounds; i++)
+            {
+                ulong lo = BitConverter.ToUInt64(kSecret.AsSpan(16 * i)) + seed64;
+                ulong hi = BitConverter.ToUInt64(kSecret.AsSpan((16 * i) + 8)) - seed64;
+                BinaryPrimitives.WriteUInt64LittleEndian(customSecret[(16 * i)..], lo);
+                BinaryPrimitives.WriteUInt64LittleEndian(customSecret[((16 * i) + 8)..], hi);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static void accumulate(Span<ulong> acc, ReadOnlySpan<byte> input, ReadOnlySpan<byte> secret, int nbStripes)
+        {
+            for (int n = 0; n < nbStripes; n++)
+            {
+                var inStripe = input[(n * stripeLength)..];
+                accumulate512(acc, inStripe, secret[(n * secretConsumeRate)..]);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static void hashLongInternalLoop(Span<ulong> acc, ReadOnlySpan<byte> input, ReadOnlySpan<byte> secret)
+        {
+            int nbStripesPerBlock = (secret.Length - stripeLength) / secretConsumeRate;
+            int blockLen = stripeLength / nbStripesPerBlock;
+            int nbBlocks = (input.Length - 1) / blockLen;
+
+            for (int n = 0; n < nbBlocks; n++)
+            {
+                accumulate(acc, input[(n * blockLen)..], secret, nbStripesPerBlock);
+                scrambleAcc(acc, secret[^stripeLength..]);
+            }
+
+            int nbStripes = (input.Length - 1 - (blockLen - nbBlocks)) / stripeLength;
+            accumulate(acc, input[(nbBlocks * blockLen)..], secret, nbStripes);
+            accumulate512(acc, input[^stripeLength..], secret[^(stripeLength + secretLastAccStart)..]);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static ulong mix2Accs(Span<ulong> acc, ReadOnlySpan<byte> secret)
+        {
+            return mul128fold64(
+                acc[0] ^ BitConverter.ToUInt64(secret),
+                acc[1] ^ BitConverter.ToUInt64(secret[8..]));
+        }
+
+        static ulong mergeAccs(Span<ulong> acc, ReadOnlySpan<byte> secret, ulong start)
+        {
+            ulong result64 = start;
+            for (int i = 0; i < 4; i++)
+            {
+                result64 += mix2Accs(acc[(2 * i)..], secret[(i * 16)..]);
+            }
+
+            return avalanche(result64);
+        }
+
+        static ulong finalizeLong64b(Span<ulong> acc, ReadOnlySpan<byte> secret, int len)
+        {
+            return mergeAccs(acc, secret[secretMergeAccsStart..], (ulong)len * prime64v1);
+        }
+
+        static ulong hashLong64Internal(ReadOnlySpan<byte> input, ReadOnlySpan<byte> secret)
+        {
+            Span<ulong> acc = [.. initAcc];
+            hashLongInternalLoop(acc, input, secret);
+            return finalizeLong64b(acc, secret, input.Length);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static ulong hashLong64WithSeedInternal(ReadOnlySpan<byte> input, ulong seed)
+        {
+            if (seed == 0)
+            {
+                return hashLong64Internal(input, kSecret);
+            }
+
+            // generate secret from seed
+            Span<byte> secret = stackalloc byte[SecretDefaultSize];
+            initCustomSecret(secret, seed);
+            return hashLong64Internal(input, secret);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static ulong hash64WithSecretAndSeedInternal(ReadOnlySpan<byte> input, ulong seed, ReadOnlySpan<byte> secret)
+        {
+            if (input.Length <= midSizeMax)
+            {
+                return hashLong64WithSeedInternal(input, seed);
+            }
+
+            return hashLong64Internal(input, secret);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static ulong hash64Internal(ReadOnlySpan<byte> input, ulong seed, ReadOnlySpan<byte> secret)
+        {
+            return input.Length switch
+            {
+                <= 16 => len0To16(input, secret, seed),
+                <= 128=> len17to128(input, secret, seed),
+                <= midSizeMax => len129to240(input, secret, seed),
+                _ => hash64WithSecretAndSeedInternal(input, seed, secret),
+            };
+        }
+    }
+}
---
+++ test/XXH3Test.cs
@@ -0,0 +1,102 @@
+﻿// Copyright (c) 2015, 2016 Sedat Kapanoglu
+// MIT License - see LICENSE file for details
+
+using System;
+using NUnit.Framework;
+
+namespace HashDepot.Test;
+
+[TestFixture]
+public partial class XXH3Test
+{
+    const ulong prime32 = 2654435761U;
+    const ulong prime64 = 11400714785074694797UL;
+
+    const int testBufferSize = 2367;
+    readonly byte[] testBuffer = new byte[testBufferSize];
+
+    [SetUp]
+    public void SetUp()
+    {
+        fillTestBuffer(testBuffer.AsSpan());
+    }
+
+    static void fillTestBuffer(Span<byte> buffer)
+    {
+        ulong byteGen = prime32;
+
+        for (int i = 0; i < buffer.Length; i++)
+        {
+            buffer[i] = (byte)(byteGen >> 56);
+            byteGen *= prime64;
+        }
+    }
+
+    static readonly object[][] seededTestData =
+    [
+        [    0,     0UL, 0x2D06800538D394C2UL ],  /* empty string */
+        [    0, prime64, 0xA8A6B918B2F0364AUL ],
+        [    1,     0UL, 0xC44BDFF4074EECDBUL ],  /*  1 -  3 */
+        [    1, prime64, 0x032BE332DD766EF8UL ],
+        [    6,     0UL, 0x27B56A84CD2D7325UL ],  /*  4 -  8 */
+        [    6, prime64, 0x84589C116AB59AB9UL ],
+        [   12,     0UL, 0xA713DAF0DFBB77E7UL ],  /*  9 - 16 */
+        [   12, prime64, 0xE7303E1B2336DE0EUL ],
+        [   24,     0UL, 0xA3FE70BF9D3510EBUL ],  /* 17 - 32 */
+        [   24, prime64, 0x850E80FC35BDD690UL ],
+        [   48,     0UL, 0x397DA259ECBA1F11UL ],  /* 33 - 64 */
+        [   48, prime64, 0xADC2CBAA44ACC616UL ],
+        [   80,     0UL, 0xBCDEFBBB2C47C90AUL ],  /* 65 - 96 */
+        [   80, prime64, 0xC6DD0CB699532E73UL ],
+        [  195,     0UL, 0xCD94217EE362EC3AUL ],  /* 129-240 */
+        [  195, prime64, 0xBA68003D370CB3D9UL ],
+
+        [  403,     0UL, 0xCDEB804D65C6DEA4UL ],  /* one block, last stripe is overlapping */
+        [  403, prime64, 0x6259F6ECFD6443FDUL ],
+        [  512,     0UL, 0x617E49599013CB6BUL ],  /* one block, finishing at stripe boundary */
+        [  512, prime64, 0x3CE457DE14C27708UL ],
+        [ 2048,     0UL, 0xDD59E2C3A5F038E0UL ],  /* 2 blocks, finishing at block boundary */
+        [ 2048, prime64, 0x66F81670669ABABCUL ],
+        [ 2099,     0UL, 0xC6B9D9B3FC9AC765UL ],  /* 2 blocks + 1 partial block, to detect off-by-one scrambling issues, like #816 */
+        [ 2099, prime64, 0x184F316843663974UL ],
+        [ 2240,     0UL, 0x6E73A90539CF2948UL ],  /* 3 blocks, finishing at stripe boundary */
+        [ 2240, prime64, 0x757BA8487D1B5247UL ],
+        [ 2367,     0UL, 0xCB37AEB9E5D361EDUL ],  /* 3 blocks, last stripe is overlapping */
+        [ 2367, prime64, 0xD2DB3415B942B42AUL ]
+    ];
+
+    static readonly object[][] customSecretTestData =
+    [
+        [ 0, 0, 0x3559D64878C5C66CUL ],  /* empty string */
+        [ 1, 0, 0x8A52451418B2DA4DUL ],  /*  1 -  3 */
+        [ 6, 0, 0x82C90AB0519369ADUL ],  /*  4 -  8 */
+        [ 12, 0, 0x14631E773B78EC57UL ],  /*  9 - 16 */
+        [ 24, 0, 0xCDD5542E4A9D9FE8UL ],  /* 17 - 32 */
+        [ 48, 0, 0x33ABD54D094B2534UL ],  /* 33 - 64 */
+        [ 80, 0, 0xE687BA1684965297UL ],  /* 65 - 96 */
+        [ 195, 0, 0xA057273F5EECFB20UL ],  /* 129-240 */
+
+        [ 403, 0, 0x14546019124D43B8UL ],  /* one block, last stripe is overlapping */
+        [ 512, 0, 0x7564693DD526E28DUL ],  /* one block, finishing at stripe boundary */
+        [ 2048, 0, 0xD32E975821D6519FUL ],  /* >= 2 blodcks, at least one scrambling */
+        [ 2367, 0, 0x293FA8E5173BB5E7UL ],  /* >= 2 blocks, at least one scrambling, last stripe unaligned */
+
+        [ 64 * 10 * 3, 0, 0x751D2EC54BC6038BUL ]   /* exactly 3 fUL blocks, not a multiple of 256 */
+    ];
+
+    static readonly object[][] generateSecretTestData =
+    [
+        [ 0, 192, new byte[] { 0xE7, 0x8C, 0x77, 0x77, 0x00 } ],
+        [ 1, 240, new byte[] { 0x2B, 0x3E, 0xDE, 0xC1, 0x00 } ],
+        [ XXHash.XXH3.MinSecretSize - 1, 277, new byte[] { 0xE8, 0x39, 0x6C, 0xCC, 0x7B } ],
+        [ XXHash.XXH3.SecretDefaultSize + 500, XXHash.XXH3.MaxSecretSize, new byte[] { 0xD6, 0x1C, 0x41, 0x17, 0xB3 } ],
+    ];
+
+    [Test]
+    [TestCaseSource(nameof(seededTestData))]
+    public void Hash64_SeededData(int len, ulong seed, ulong expectedResult)
+    {
+        var result = XXHash.XXH3.Hash64(testBuffer.AsSpan(0, len), seed);
+        Assert.That(result, Is.EqualTo(expectedResult), $"Failed for len={len}, seed={seed}");
+    }
+}
